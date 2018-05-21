6ed7e59cac4f3f268ecccf7a13ae5bd628492a1e
Luca Montanari <luca.montanari@selecta.it>
2018-05-21 23:27:47 +0200

    Added MediaInfo class for future (?) Media controls implementation

---
+++ src/WinSteroid.Common/Models/MediaInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace WinSteroid.Common.Models
+{
+    public class MediaInfo
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Album { get; set; }
+    }
+}
